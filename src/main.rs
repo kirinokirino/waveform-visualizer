2b5b9b3b315164459e5a1be006192500a7abc5fa
kirinokirino <kirinokirino@lunariko.com>
2023-02-06 14:31:16 +0300

    Swap macroquad for speedy2d.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,87 @@
+#[warn(clippy::nursery, clippy::pedantic)]
+use speedy2d;
+use speedy2d::{
+    color::Color,
+    dimen::{UVec2, Vec2},
+    window::{
+        KeyScancode, VirtualKeyCode, WindowCreationOptions, WindowHandler, WindowHelper,
+        WindowPosition, WindowSize,
+    },
+    Graphics2D, Window,
+};
 
-use macroquad::prelude::*;
-use macroquad::rand::rand;
+const WINDOW_WIDTH: u32 = 600;
+const WINDOW_HEIGHT: u32 = 480;
 
-const WINDOW_WIDTH: u16 = 600;
-const WINDOW_HEIGHT: u16 = 480;
+fn main() {
+    let window_size = UVec2::new(WINDOW_WIDTH, WINDOW_HEIGHT);
+    let window_pixels = WindowSize::PhysicalPixels(window_size);
+    let window = Window::new_with_options(
+        "FLOATING",
+        WindowCreationOptions::new_windowed(window_pixels, Some(WindowPosition::Center))
+            .with_decorations(false)
+            .with_transparent(true),
+    )
+    .expect("Wasn't able to create a window!");
+    window.run_loop(App::new(window_size));
+}
 
-fn window_conf() -> Conf {
-    Conf {
-        window_title: "FLOATING".to_string(),
-        window_width: WINDOW_WIDTH.into(),
-        window_height: WINDOW_HEIGHT.into(),
-        fullscreen: false,
-        ..Default::default()
+struct App {
+    viewport: UVec2,
+}
+
+impl App {
+    pub fn new(window_size: UVec2) -> Self {
+        Self {
+            viewport: window_size,
+        }
     }
 }
 
+impl WindowHandler for App {
+    fn on_draw(&mut self, helper: &mut WindowHelper<()>, graphics: &mut Graphics2D) {
+        let ww = WINDOW_WIDTH as f32;
+        let wh = WINDOW_HEIGHT as f32;
+        let points = 36 * 4;
 
-#[macroquad::main(window_conf)]
-async fn main() {
-	let ww = WINDOW_WIDTH as f32;
-	let wh = WINDOW_HEIGHT as f32;
-	let points = 36 * 4;
-	
-    loop {
-    	println!("{}", get_fps());
-		let mut wave: Vec<Vec2> = Vec::with_capacity(points);
-		for point in 0..points {
-			wave.push(Vec2::new((ww / points as f32 * point as f32), wh / 2.0 + (rand() % 100) as f32 - 50.0));
-		}
-        clear_background(LIGHTGRAY);
+        let mut wave: Vec<Vec2> = Vec::with_capacity(points);
+        for point in 0..points {
+            wave.push(Vec2::new(
+                ww / points as f32 * point as f32,
+                wh / 2.0 + (fastrand::f32() * 100.0) - 50.0,
+            ));
+        }
 
-		for pair in wave.as_slice().windows(2) {
-			let (from, to) = (pair[0], pair[1]);
-			let (x1, y1, x2, y2) = (from.x, from.y, to.x, to.y);
-			draw_line(x1, y1, x2, y2, 1.0, BLACK);
-		}
-		
-        draw_text("HELLO", 20.0, 20.0, 30.0, DARKGRAY);
+        graphics.clear_screen(Color::from_rgb(0.8, 0.8, 0.8));
 
-        std::thread::sleep(std::time::Duration::from_millis(16));
-        next_frame().await
+        for pair in wave.as_slice().windows(2) {
+            let (from, to) = (pair[0], pair[1]);
+            graphics.draw_line(from, to, 2.0, Color::BLACK)
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(60));
+        helper.request_redraw();
+    }
+
+    fn on_resize(&mut self, _helper: &mut WindowHelper<()>, size_pixels: UVec2) {
+        self.viewport = size_pixels;
+    }
+
+    fn on_mouse_move(&mut self, _helper: &mut WindowHelper<()>, position: Vec2) {
+        //self.mouse_pos = position;
+    }
+
+    fn on_key_down(
+        &mut self,
+        helper: &mut WindowHelper<()>,
+        virtual_key_code: Option<VirtualKeyCode>,
+        scancode: KeyScancode,
+    ) {
+        if let Some(key_code) = virtual_key_code {
+            match key_code {
+                VirtualKeyCode::Escape => helper.terminate_loop(),
+                key => println!("Key: {key:?}, scancode: {scancode}"),
+            }
+        }
     }
 }
