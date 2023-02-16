ac107f3ee7e120eaec38efcb7a7f5512ffada253
kirinokirino <kirinokirino@lunariko.com>
2023-02-17 00:23:37 +0300

    show must go on.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ use lewton::inside_ogg::OggStreamReader;
 
 use std::fs::File;
 
-
 const WINDOW_WIDTH: u32 = 600;
 const WINDOW_HEIGHT: u32 = 480;
 
@@ -34,7 +33,7 @@ fn main() {
 struct App {
     viewport: UVec2,
     audio: Audio,
-    next: bool,
+    segment: i32,
 }
 
 impl App {
@@ -43,39 +42,44 @@ impl App {
         Self {
             viewport: window_size,
             audio,
-            next: true,
+            segment: 0,
         }
     }
 }
 
 impl WindowHandler for App {
     fn on_draw(&mut self, helper: &mut WindowHelper<()>, graphics: &mut Graphics2D) {
-        if self.next {
-            self.next = false;
-            let buffer = self.audio.next();
-            if let Some(buffer) = buffer {
-                let points = 512_usize;
-                let mut wave: Vec<Vec2> = vec![Vec2::ZERO; points];
-                buffer
-                    .first_channel
-                    .iter()
-                    .enumerate()
-                    .map(|(i, sample)| {
-                        (i, Vec2::new(
-                            WINDOW_WIDTH as f32 / 512.0 * i as f32,
-                            f32::from(*sample).mul_add((WINDOW_HEIGHT as f32 * 0.5) / f32::from(i16::MAX), WINDOW_HEIGHT as f32 / 2.0),
-                        ))
-                    })
-                    .for_each(|(i, sample)| if let Some(point) = wave.get_mut(i) {
-						*point = sample;
-                    });
-                graphics.clear_screen(Color::from_rgb(0.8, 0.8, 0.8));
-                for pair in wave.as_slice().windows(2) {
-                    let (from, to) = (pair[0], pair[1]);
-                    graphics.draw_line(from, to, 2.0, Color::BLACK);
-                }
-            }
+        let segment = if self.segment > 0 {
+            self.segment as usize
+        } else {
+            0
+        };
+        let segment_size = self.audio.meta.sample_rate as usize / 4;
+        let slice = segment_size * segment;
+        let wave: Vec<Vec2> = self
+            .audio
+            .buffer
+            .iter()
+            .skip(slice)
+            .take(segment_size)
+            .enumerate()
+            .map(|(i, sample)| {
+                Vec2::new(
+                    WINDOW_WIDTH as f32 / 512.0 * i as f32,
+                    f32::from(*sample).mul_add(
+                        (WINDOW_HEIGHT as f32 * 0.5) / f32::from(i16::MAX),
+                        WINDOW_HEIGHT as f32 / 2.0,
+                    ),
+                )
+            })
+            .collect();
+
+        graphics.clear_screen(Color::from_rgb(0.8, 0.8, 0.8));
+        for pair in wave.as_slice().windows(2) {
+            let (from, to) = (pair[0], pair[1]);
+            graphics.draw_line(from, to, 2.0, Color::BLACK);
         }
+
         std::thread::sleep(std::time::Duration::from_millis(10));
         helper.request_redraw();
     }
@@ -93,7 +97,7 @@ impl WindowHandler for App {
         if let Some(key_code) = virtual_key_code {
             match key_code {
                 VirtualKeyCode::Escape => helper.terminate_loop(),
-                VirtualKeyCode::Space => self.next = true,
+                VirtualKeyCode::Space => self.segment += 1,
                 key => println!("Key: {key:?}, scancode: {scancode}"),
             }
         }
@@ -101,8 +105,8 @@ impl WindowHandler for App {
 }
 
 struct Audio {
-    counter: usize,
-    buffers: Vec<Buffer>,
+    buffer: Vec<i16>,
+    meta: Meta,
 }
 
 impl Audio {
@@ -113,63 +117,37 @@ impl Audio {
         let mut stream_reader =
             OggStreamReader::new(file).expect("Can't create oggstreamreader for file.");
         let sample_rate = stream_reader.ident_hdr.audio_sample_rate;
-        println!("Sample rate: {sample_rate}");
         let audio_channels = stream_reader.ident_hdr.audio_channels;
-        println!("There are {audio_channels} audio channels.");
-
-        let mut buffers = Vec::new();
-        let mut packet_idx = 0;
-        let mut total_samples = 0;
+        let mut buffer = Vec::new();
         while let Some(samples) = stream_reader
             .read_dec_packet_itl()
             .expect("couldn't read ogg packet")
         {
-            println!(
-                "Decoded packet no {}, with {} samples.",
-                packet_idx,
-                samples.len()
-            );
-            packet_idx += 1;
-            total_samples += samples.len();
-            match audio_channels {
-                1 => {
-                    let mut buffer = Buffer::new();
-                    buffer.mono_buffer(&samples);
-                    buffers.push(buffer);
-                }
-                n => panic!("unsupported number of channels: {n}"),
-            };
+            buffer.extend_from_slice(&samples);
         }
-        let length_in_seconds = total_samples as f32 / (f32::from(audio_channels) * sample_rate as f32);
-        println!("The piece is {length_in_seconds} s long.");
-        Self {
-            buffers,
-            counter: 0,
-        }
-    }
-    pub fn next(&mut self) -> Option<&Buffer> {
-        let result = if self.counter >= self.buffers.len() {
-            None
-        } else {
-            self.buffers.get(self.counter)
+        let length_in_seconds =
+            buffer.len() as f32 / (f32::from(audio_channels) * sample_rate as f32);
+
+        let meta = Meta {
+            length: length_in_seconds,
+            channels: audio_channels,
+            sample_rate,
         };
-        self.counter += 1;
-        result
+        meta.print();
+        Self { buffer, meta }
     }
 }
 
-struct Buffer {
-    pub first_channel: Vec<i16>,
+struct Meta {
+    length: f32,
+    channels: u8,
+    sample_rate: u32,
 }
 
-impl Buffer {
-    pub const fn new() -> Self {
-        Self {
-            first_channel: Vec::new(),
-        }
-    }
-
-    fn mono_buffer(&mut self, samples: &[i16]) {
-        self.first_channel = samples.to_vec();
+impl Meta {
+    pub fn print(&self) {
+        println!("The piece is {}s long.", self.length);
+        println!("Sample rate: {}", self.sample_rate);
+        println!("There are {} audio channels.", self.channels);
     }
 }
